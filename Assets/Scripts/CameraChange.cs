fb0bf650afa03152294d5cb0a6f704568b8c97a1
Orosz <oroszandrei@gmail.com>
2018-05-09 13:05:18 +0300

    Commit description

---
+++ Assets/Scripts/CameraChange.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour {
+
+    public GameObject NormalCam;
+    public GameObject FarCam;
+    public GameObject FPCam;
+    public GameObject TwoDimCam;
+    public int CamMode;
+
+	void Update () {
+        if (Input.GetButtonDown("Viewmode"))
+        {
+            if (CamMode == 3)
+            {
+                CamMode = 0;
+            }
+            else
+            {
+                CamMode += 1;
+            }
+            StartCoroutine(ModeChange());
+        }
+    }
+
+    IEnumerator ModeChange()
+    {
+        yield return new WaitForSeconds(0.01f);
+        if (CamMode == 0)
+        {
+            NormalCam.SetActive(true);
+            FPCam.SetActive(false);
+            FarCam.SetActive(false);
+            TwoDimCam.SetActive(false);
+        }
+        if (CamMode == 1)
+        {
+            FarCam.SetActive(true);
+            NormalCam.SetActive(false);
+            TwoDimCam.SetActive(false);
+        }
+        if (CamMode == 2)
+        {
+            FPCam.SetActive(true);
+            FarCam.SetActive(false);
+            NormalCam.SetActive(false);
+            TwoDimCam.SetActive(false);
+        }
+        if (CamMode == 3)
+        {
+            TwoDimCam.SetActive(true);
+            FPCam.SetActive(false);
+            FarCam.SetActive(false);
+            NormalCam.SetActive(false);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Dreamcar01Track.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dreamcar01Track : MonoBehaviour {
+
+    public GameObject TheMarker;
+    public GameObject[] TrackMarks = new GameObject[61];
+    public int MarkTracker;
+
+	// Update is called once per frame
+	void Update () {
+
+        for (int i = 0; i < TrackMarks.Length; i++)
+        {
+            if (MarkTracker == i)
+            {
+                TheMarker.transform.position = TrackMarks[i].transform.position;
+            }
+
+        }
+
+    }
+
+    IEnumerator OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Dreamcar01")
+        {
+            this.GetComponent<BoxCollider>().enabled = false;
+            MarkTracker += 1;
+            if (MarkTracker == 60)
+            {
+                MarkTracker = 0;
+            }
+            yield return new WaitForSeconds(1);
+            this.GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+}
